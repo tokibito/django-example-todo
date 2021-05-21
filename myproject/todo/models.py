624251c570573d80c627805664aee3ce5676ff5b
atthaboon.s <atthaboon.s@qahive.com>
2021-05-21 07:36:25 +0700

    Change to eng lang

--- myproject/todo/models.py
+++ myproject/todo/models.py
@@ -6,9 +6,9 @@ class Todo(models.Model):
     TODOのデータを扱うモデルクラス
     このクラスのインスタンス1つが、データベース上の1レコードに相当します
     """
-    name = models.CharField("名称", max_length=50)
-    done = models.BooleanField("完了")
-    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+    name = models.CharField("Name", max_length=50)
+    done = models.BooleanField("Done")
+    created_at = models.DateTimeField("Created_at", auto_now_add=True)
 
     def __str__(self):
         return self.name
